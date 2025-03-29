e368fa97c450eb8270b0af6ba7648fab8c09ab96
mohmdloai <20812016100937@eng.zu.edu.eg>
2025-03-29 10:27:49 +0200

    Implemented useMutation

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,9 +1,19 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import React from "react";
 import Loader from "./Loader";
 // import '../index.css'
 
 const Todo = () => {
+  const getMutation = useMutation({
+    mutationFn: async () =>
+      await fetch("https://jsonplaceholder.typicode.com/todos").then((res) =>
+        res.json()
+      ),
+    onSuccess: () => {
+      console.log("Data fetched successfully");
+    },
+  });
+  /*
   const { data,isLoading } = useQuery({
     queryKey: ["todos"],
     queryFn: async () =>
@@ -11,18 +21,32 @@ const Todo = () => {
         res.json()
       ),
   });
+*/
+  if (getMutation.isPending) {
+    return <Loader />;
+  }
+  console.log(getMutation);
+  // console.log(getMutation.data);
+  // console.log(getMutation.status);
 
-  if (isLoading ){return <Loader />}
   return (
-    <div className="container">
-      {data &&
-        data?.map((todo) => (
-          <div className={`todo ${todo.completed ? "Completed" : "Pending"}`} key={todo.id}>
-            {todo.title}
-            <p className= {todo.completed ? "Done" : "Open"}>Status: {todo.completed ? "Done" : "Open"}</p>
-          </div>
-        ))}
-    </div>
+    <>
+      <button onClick={() => getMutation.mutate()}>Fetch Todos</button>
+      <div className="container">
+        {getMutation.data &&
+          getMutation.data?.map((todo) => (
+            <div
+              className={`todo ${todo.completed ? "Completed" : "Pending"}`}
+              key={todo.id}
+            >
+              {todo.title}
+              <p className={todo.completed ? "Done" : "Open"}>
+                Status: {todo.completed ? "Done" : "Open"}
+              </p>
+            </div>
+          ))}
+      </div>
+    </>
   );
 };
 
